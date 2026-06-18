a3ff0e7322875ddecbd2af86df2c81072c2cb4d2
Laxita Jain <laxitajain912@gmail.com>
2026-06-18 20:59:16 +0400

    fix: nav link areas

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -7,11 +7,13 @@ export default function Navigation() {
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         {navLinks.map((link) => (
-          <li
-            className="h-10 px-4 py-2 hover:text-accent-20 transition-colors cursor-pointer hidden sm:block"
-            key={link.to}
-          >
-            <Link href={link.to}>{link.label}</Link>
+          <li className="hidden sm:block" key={link.to}>
+            <Link 
+              href={link.to}
+              className="inline-flex h-10 px-4 py-2 hover:text-accent-20 transition-colors cursor-pointer items-center"
+            >
+              {link.label}
+            </Link>
           </li>
         ))}
         <li className="mx-2 sm:hidden">
