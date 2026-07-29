a0f9ec1f56d1814d9d81aa0d5675d997d2557259
laxita <87566518+laxitajain@users.noreply.github.com>
2026-07-29 23:18:38 +0530

    Update WavyBanner.js

--- app/_components/WavyBanner.js
+++ app/_components/WavyBanner.js
@@ -76,16 +76,7 @@ export default function WavyBanner() {
                 rel="noopener noreferrer" 
                 className="hover:fill-white transition-colors duration-300"
               >
-                Check out my Codolio Profile ↗ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              </a>
-               <a 
-                key={i} 
-                href="https://ygc.sachinchaurasiya.dev/laxitajain" 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className="hover:fill-white transition-colors duration-300"
-              >
-                GitHub Stats ↗ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Check out my Codolio Profile ↗ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GitHub Stats ↗ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               </a>
             ))}
           </textPath>
